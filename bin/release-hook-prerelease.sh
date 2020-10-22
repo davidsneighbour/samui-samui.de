f8803e6339acbdf886cee8f17571561355947125
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-10-22 20:29:37 +0700

    chore: removing textlint and remark from repo and add go.mod script
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

---
+++ bin/release-hook-prerelease.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cp bin/go.mod.repo go.mod
+
+hugo mod get -u ./...
+hugo mod tidy
+hugo mod vendor
+
+cp go.mod bin/go.mod.repo
+
+git add _vendor
+git add go.mod
+git add go.sum
+
+{
+  echo 'replace github.com/davidsneighbour/dnb-hugo-garuda => /home/patrick/Projects/Hugo/dnb-hugo-garuda'
+  echo 'replace github.com/davidsneighbour/dnb-hugo/netlification => /home/patrick/Projects/Hugo/dnb-hugo/netlification'
+  echo 'replace github.com/davidsneighbour/dnb-hugo/robots => /home/patrick/Projects/Hugo/dnb-hugo/robots'
+  echo 'replace github.com/davidsneighbour/dnb-hugo/shortcodes => /home/patrick/Projects/Hugo/dnb-hugo/shortcodes'
+  echo 'replace github.com/davidsneighbour/dnb-hugo-libs/bootstrap4 => /home/patrick/Projects/Hugo/dnb-hugo-libs/bootstrap4'
+} >> go.mod
