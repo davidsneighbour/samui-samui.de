b0847643eb3309050fb304c4c43c815bdb68f687
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-11-24 19:07:35 +0700

    chore: cleanup check script

--- bin/check.sh
+++ bin/check.sh
@@ -2,16 +2,16 @@
 
 # access "${arr[0]}", "${arr[1]}"
 declare -a arr=(
-  "../../Hugo/dnb-hugo"
-  "../dnb-hugo-garuda"
-  "../dnb-hugo-garuda-template"
-  "../dnb-hugo-libs"
-  "../../Me/dnb-hugo-namespace"
-  "../samui-samui.de"
+  "Hugo/dnb-hugo"
+  "Hugo/dnb-hugo-garuda"
+  "Hugo/dnb-hugo-garuda-template"
+  "Hugo/dnb-hugo-libs"
+  "Me/dnb-hugo-namespace"
+  "Me/samui-samui.de"
 )
 
 for i in "${arr[@]}"
 do
-  cd "$i" || exit
+  cd ../../"$i" || exit
   echo "$i": "$(git status --porcelain=1 | wc -l)" changes
 done
