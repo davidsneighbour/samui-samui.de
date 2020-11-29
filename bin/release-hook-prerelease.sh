8d05f0eba45abd516da213b7672219e2efac007b
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-11-29 17:46:22 +0700

    chore: revert changes

--- bin/release-hook-prerelease.sh
+++ bin/release-hook-prerelease.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
-cp bin/go.mod.repo go.mod
+declare -a MODULES=(
+  'github.com/davidsneighbour/dnb-hugo-garuda'
+  'github.com/davidsneighbour/dnb-hugo/favicon'
+  'github.com/davidsneighbour/dnb-hugo/netlification'
+  'github.com/davidsneighbour/dnb-hugo/renderhooks'
+  'github.com/davidsneighbour/dnb-hugo/robots'
+  'github.com/davidsneighbour/dnb-hugo/sitemap'
+  'github.com/davidsneighbour/dnb-hugo/shortcodes'
+  'github.com/davidsneighbour/dnb-hugo-libs/bootstrap5'
+  'github.com/davidsneighbour/dnb-hugo-libs/bootstrap-icons'
+  'github.com/davidsneighbour/dnb-hugo-libs/popper.js'
+)
+declare -a REPLACE=(
+  '/home/patrick/Projects/Hugo/dnb-hugo-garuda'
+  '/home/patrick/Projects/Hugo/dnb-hugo/favicon'
+  '/home/patrick/Projects/Hugo/dnb-hugo/netlification'
+  '/home/patrick/Projects/Hugo/dnb-hugo/renderhooks'
+  '/home/patrick/Projects/Hugo/dnb-hugo/robots'
+  '/home/patrick/Projects/Hugo/dnb-hugo/sitemap'
+  '/home/patrick/Projects/Hugo/dnb-hugo/shortcodes'
+  '/home/patrick/Projects/Hugo/dnb-hugo-libs/bootstrap5'
+  '/home/patrick/Projects/Hugo/dnb-hugo-libs/bootstrap-icons'
+  '/home/patrick/Projects/Hugo/dnb-hugo-libs/popper.js'
+)
+
+for i in "${MODULES[@]}"
+do
+  go mod edit -dropreplace $i
+done
 
 hugo mod get -u ./...
 hugo mod tidy
 rm -rf public
-#hugo mod vendor
 
-cp go.mod bin/go.mod.repo
-
-#git add _vendor
 git add go.mod
 git add go.sum
 
-{
-  echo 'replace github.com/davidsneighbour/dnb-hugo-garuda => /home/patrick/Projects/Hugo/dnb-hugo-garuda'
-  echo 'replace github.com/davidsneighbour/dnb-hugo/netlification => /home/patrick/Projects/Hugo/dnb-hugo/netlification'
-  echo 'replace github.com/davidsneighbour/dnb-hugo/renderhooks => /home/patrick/Projects/Hugo/dnb-hugo/renderhooks'
-  echo 'replace github.com/davidsneighbour/dnb-hugo/robots => /home/patrick/Projects/Hugo/dnb-hugo/robots'
-  echo 'replace github.com/davidsneighbour/dnb-hugo/favicons => /home/patrick/Projects/Hugo/dnb-hugo/favicons'
-  echo 'replace github.com/davidsneighbour/dnb-hugo/shortcodes => /home/patrick/Projects/Hugo/dnb-hugo/shortcodes'
-  echo 'replace github.com/davidsneighbour/dnb-hugo-libs/bootstrap5 => /home/patrick/Projects/Hugo/dnb-hugo-libs/bootstrap5'
-  echo 'replace github.com/davidsneighbour/dnb-hugo-libs/bootstrap-icons => /home/patrick/Projects/Hugo/dnb-hugo-libs/bootstrap-icons'
-} >> go.mod
+for i in "${!MODULES[@]}"; do
+  go mod edit -replace ${MODULES[$i]}=${REPLACE[$i]}
+done
 
 hugo --gc --minify
 git add -f public
