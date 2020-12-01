0bd4f61bd262b2361016490f1144061bc86dc1b8
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-12-01 19:48:52 +0700

    chore: de-complicate netlify.toml

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 npm install -g @babel/cli @babel/core @babel/preset-env
-#git config --global --add url."git@github.com:".insteadOf "https://github.com/"
+# git config --global --add url."git@github.com:".insteadOf "https://github.com/"
 hugo mod get -u ./...
 hugo
 rm -rf public/posts
