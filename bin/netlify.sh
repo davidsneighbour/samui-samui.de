62e8feb69d0242cea954dd8249d0016e351a6a1e
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-01-02 20:29:51 +0700

    fix: remove github access change

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -8,7 +8,7 @@
 # leaving this git config line in. sometimes you want to access a private repo
 # and in that case it's better if you access it via ssh instead of https.
 # this config line does configure your Netlify session to do that.
-git config --global --add url."git@github.com:".insteadOf "https://github.com/"
+#git config --global --add url."git@github.com:".insteadOf "https://github.com/"
 
 # updating modules
 hugo mod get -u ./...
