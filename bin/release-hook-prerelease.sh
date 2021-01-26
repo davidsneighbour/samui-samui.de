645c35b8158c6a8454623072deee6c2b079adea3
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-01-26 19:20:55 +0700

    chore: fix build system

--- bin/release-hook-prerelease.sh
+++ bin/release-hook-prerelease.sh
@@ -1,44 +1,12 @@
 #!/bin/bash
 
-declare -a MODULES=(
-  'github.com/dnb-hugo/debugprint'
-  'github.com/dnb-hugo/garuda'
-  'github.com/dnb-hugo/components/favicon'
-  'github.com/dnb-hugo/components/functions'
-  'github.com/dnb-hugo/components/netlification'
-  'github.com/dnb-hugo/components/opensearch'
-  'github.com/dnb-hugo/components/renderhooks'
-  'github.com/dnb-hugo/components/robots'
-  'github.com/dnb-hugo/components/search-algolia'
-  'github.com/dnb-hugo/components/sitemap'
-  'github.com/dnb-hugo/shortcodes'
-  'github.com/dnb-hugo/blocks/bootstrap5'
-  'github.com/dnb-hugo/libraries/bootstrap5'
-  'github.com/dnb-hugo/libraries/bootstrap-icons'
-  'github.com/dnb-hugo/libraries/popper.js'
-)
-declare -a REPLACE=(
-  '/home/patrick/Projects/dnb-hugo/debugprint'
-  '/home/patrick/Projects/dnb-hugo/garuda'
-  '/home/patrick/Projects/dnb-hugo/components/favicon'
-  '/home/patrick/Projects/dnb-hugo/components/functions'
-  '/home/patrick/Projects/dnb-hugo/components/netlification'
-  '/home/patrick/Projects/dnb-hugo/components/opensearch'
-  '/home/patrick/Projects/dnb-hugo/components/renderhooks'
-  '/home/patrick/Projects/dnb-hugo/components/robots'
-  '/home/patrick/Projects/dnb-hugo/components/search-algolia'
-  '/home/patrick/Projects/dnb-hugo/components/sitemap'
-  '/home/patrick/Projects/dnb-hugo/shortcodes'
-  '/home/patrick/Projects/dnb-hugo/blocks/bootstrap5'
-  '/home/patrick/Projects/dnb-hugo/libraries/bootstrap5'
-  '/home/patrick/Projects/dnb-hugo/libraries/bootstrap-icons'
-  '/home/patrick/Projects/dnb-hugo/libraries/popper.js'
-)
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-for i in "${MODULES[@]}"
-do
-  go mod edit -dropreplace "$i"
-done
+if test -f "$SCRIPTPATH"/replacements; then
+  while read -ra __; do
+    go mod edit -dropreplace ${__[0]}
+  done < "$SCRIPTPATH"/replacements
+fi
 
 hugo mod get -u ./...
 hugo mod tidy
@@ -50,6 +18,8 @@ git add go.sum
 #hugo
 #git add public/ -f
 
-for i in "${!MODULES[@]}"; do
-  go mod edit -replace "${MODULES[$i]}"="${REPLACE[$i]}"
-done
+if test -f "$SCRIPTPATH"/replacements; then
+  while read -ra __; do
+    go mod edit -replace ${__[0]}=${__[1]}
+  done < "$SCRIPTPATH"/replacements
+fi
