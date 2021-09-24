a04b66751c5930a76813133a928338ec2ecca54b
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-09-24 23:19:46 +0700

    chore(config): getting the dev server to run again

--- bin/hugo-build-project.sh
+++ bin/hugo-build-project.sh
@@ -3,29 +3,19 @@
 # cleanup hugo loggin
 npm run clean:hugo
 
-# starting hugo server
-hugo server \
+# build project
+hugo \
     --gc \
-    #--bind 192.168.1.201 \
-    #--port 1313 \
     --environment development \
-    --disableFastRender \
     --i18n-warnings \
-    --navigateToChanged \
     --templateMetrics \
     --templateMetricsHints \
     --path-warnings \
-    --poll 1s \
     --cleanDestinationDir \
-    --renderToDisk \
     --buildDrafts --buildExpired --buildFuture \
-    --watch \
     --enableGitInfo \
-    --forceSyncStatic \
-    --log true --logFile hugo.log \
+    --log=true --logFile hugo.log \
     --verbose \
     --verboseLog \
     --debug \
-    --meminterval 5s \
-    --memstats hugo_memory.log \
     --print-mem
