51a64fa0c2d79cb737fbf124d2d9df3e96240059
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-09-24 21:02:35 +0700

    chore(build): improve server script

--- bin/hugo-dev-server.sh
+++ bin/hugo-dev-server.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
+# cleanup hugo loggin
+npm run clean:hugo
+
+# starting hugo server
 hugo server \
-    --minify  \
-    --disableFastRender  \
-    --i18n-warnings  \
-    --navigateToChanged   \
-    --templateMetrics  \
-    --templateMetricsHints  \
-    --verboseLog  \
-    --path-warnings  \
-    --buildDrafts --buildExpired --buildFuture  \
-    --watch  \
-    --enableGitInfo  \
-    --forceSyncStatic  \
-    --log true --logFile hugo.log  \
-    --verbose
+    --gc \
+    --bind 192.168.1.201 \
+    --port 1313 \
+    --environment development \
+    --disableFastRender \
+    --i18n-warnings \
+    --navigateToChanged \
+    --templateMetrics \
+    --templateMetricsHints \
+    --path-warnings \
+    --poll 1s \
+    --cleanDestinationDir \
+    --renderToDisk \
+    --buildDrafts --buildExpired --buildFuture \
+    --watch \
+    --enableGitInfo \
+    --forceSyncStatic \
+    --log true --logFile hugo.log \
+    --verbose \
+    --verboseLog \
+    --debug \
+    --meminterval 5s \
+    --memstats hugo_memory.log \
+    --print-mem
