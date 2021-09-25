acb75a1001c8995812718961bbeb80dd7b615eba
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-09-25 22:26:23 +0700

    chore(scripts): shell check pre release script

--- bin/release-hook-prerelease.sh
+++ bin/release-hook-prerelease.sh
@@ -6,18 +6,18 @@ REQUIRED_TOOLS=(
   go
 )
 
-for tool in ${REQUIRED_TOOLS[@]}; do
-  if ! command -v ${tool} >/dev/null; then
-    echo "${tool} is required... "
+for TOOL in "${REQUIRED_TOOLS[@]}"; do
+  if ! command -v "${TOOL}" >/dev/null; then
+    echo "${TOOL} is required... "
     exit 1
   fi
 done
 
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 
 if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
-    go mod edit -dropreplace ${__[0]}
+    go mod edit -dropreplace "${__[0]}"
   done < "$SCRIPTPATH"/replacements
 fi
 
@@ -38,7 +38,7 @@ git add go.sum --force
 
 if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
-    go mod edit -replace ${__[0]}=${__[1]}
+    go mod edit -replace "${__[0]}"="${__[1]}"
   done < "$SCRIPTPATH"/replacements
 fi
 
