cdf42a1b8604af7a2082eea4a98b7dca23b4eb58
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-09-26 19:05:00 +0700

    chore: update build scripts

--- bin/hugo-dev-server.sh
+++ bin/hugo-dev-server.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+REQUIRED_TOOLS=(
+  hugo
+  npm
+)
+
+for TOOL in "${REQUIRED_TOOLS[@]}"; do
+  if ! command -v "${TOOL}" >/dev/null; then
+    echo "${TOOL} is required... "
+    exit 1
+  fi
+done
+
 # cleanup hugo loggin
 npm run clean:hugo
 
@@ -26,6 +38,7 @@ hugo server \
     --debug \
     --meminterval 5s \
     --memstats hugo_memory.log \
-    --print-mem
-    #--bind 192.168.1.201 \
-    #--port 1313 \
+    --print-mem \
+    --bind 192.168.1.201 \
+    --port 1313 \
+    --baseURL http://192.168.1.201 
