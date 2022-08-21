cd51b621678c62ba3c9aa8ec17bd6e89a6036a5f
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-08-21 16:30:08 +0700

    chore(config): fix standard version config and remove changelog

--- .versionrc.js
+++ .versionrc.js
@@ -1,2 +1,21 @@
 const defaultStandardVersion = require("@davidsneighbour/config/standard-version");
 module.exports = defaultStandardVersion;
+
+const localStandardVersion = {
+  bumpFiles: [
+    ...defaultStandardVersion.bumpFiles,
+    {
+      filename: 'data/dnb/garuda/build.json',
+      type: 'json',
+    },
+  ],
+  skip: {
+    changelog: true
+  }
+};
+
+const standardVersion = {
+  ...defaultStandardVersion,
+  ...localStandardVersion,
+};
+module.exports = standardVersion;
