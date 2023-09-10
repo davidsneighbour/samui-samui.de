0c01649f2b03d48e28e231916ddd3f62b9772609
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-09-10 14:10:03 +0700

    chore(config): update configuration and dependency configuration

--- .versionrc.js
+++ .versionrc.js
@@ -1,21 +1,21 @@
-const defaultStandardVersion = require("@davidsneighbour/config/standard-version");
+const defaultStandardVersion = require("@davidsneighbour/standard-version-config");
 module.exports = defaultStandardVersion;
 
 const localStandardVersion = {
-  bumpFiles: [
-    ...defaultStandardVersion.bumpFiles,
-    {
-      filename: 'data/dnb/garuda/build.json',
-      type: 'json',
-    },
-  ],
-  skip: {
-    changelog: true
-  }
+	bumpFiles: [
+		...defaultStandardVersion.bumpFiles,
+		{
+			filename: 'data/dnb/garuda/build.json',
+			type: 'json',
+		},
+	],
+	skip: {
+		changelog: true
+	}
 };
 
 const standardVersion = {
-  ...defaultStandardVersion,
-  ...localStandardVersion,
+	...defaultStandardVersion,
+	...localStandardVersion,
 };
 module.exports = standardVersion;
