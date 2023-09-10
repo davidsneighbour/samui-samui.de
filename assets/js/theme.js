283c15b0993bbcfa316ead8111c36a6940fb230f
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-09-10 19:40:38 +0700

    fix: various fixes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,5 @@
 import lazySizes from 'libs/lazysizes';
-import dayjs from  'js/dayjs';
+import dayjs from 'js/dayjs';
 
 // https://github.com/aFarkas/lazysizes
 // needs to be called explicitly, because webpack can't see into uncompiled hugo
@@ -10,34 +10,33 @@ lazySizes.init();
  ******************************************************************************/
 let elements = document.querySelectorAll(".is--datediff");
 Array.prototype.forEach.call(elements, function (el) {
-  let $date1 = dayjs(el.getAttribute("data-date"));
-  let $date2 = dayjs(new Date());
-  let $type = el.getAttribute("data-type");
-  if ($type === "months") {
-    el.textContent = parseInt($date2.diff($date1, "months"), 10);
-  } else {
-    el.textContent = $date2.diff($date1, "days") + 1;
-  }
+	let $date1 = dayjs(el.getAttribute("data-date"));
+	let $date2 = dayjs(new Date());
+	let $type = el.getAttribute("data-type");
+	if ($type === "months") {
+		el.textContent = parseInt($date2.diff($date1, "months"), 10);
+	} else {
+		el.textContent = $date2.diff($date1, "days") + 1;
+	}
 });
-
 
 /*******************************************************************************
  * make navbar static on scroll
  ******************************************************************************/
 let bottom = false;
 window.addEventListener("scroll", function () {
-  "use strict";
-  let menu = document.getElementById("topnavigation");
-  let offset = menu.getBoundingClientRect();
-  if (bottom === false) {
-    bottom = offset.top;
-  }
-  let bodyOffset = window.pageYOffset || document.documentElement.scrollTop;
-  if (bodyOffset > bottom) {
-    menu.classList.add("navbar-fixed-top");
-  } else {
-    menu.classList.remove("navbar-fixed-top");
-  }
+	"use strict";
+	let menu = document.getElementById("topnavigation");
+	let offset = menu.getBoundingClientRect();
+	if (bottom === false) {
+		bottom = offset.top;
+	}
+	let bodyOffset = window.pageYOffset || document.documentElement.scrollTop;
+	if (bodyOffset > bottom) {
+		menu.classList.add("navbar-fixed-top");
+	} else {
+		menu.classList.remove("navbar-fixed-top");
+	}
 });
 
 /*******************************************************************************
@@ -50,17 +49,17 @@ backToTopLink.id = "back-to-top";
 backToTopLink.innerHTML = "Zur&uuml;ck nach oben";
 document.body.appendChild(backToTopLink);
 window.addEventListener("scroll", function () {
-  let backToTopLinkDiv = document.getElementById("back-to-top");
-  let location = document.body.getBoundingClientRect();
-  if (Math.abs(location.top) > offset) {
-    backToTopLinkDiv.classList.add("d-block");
-    backToTopLinkDiv.classList.remove("d-none");
-  } else {
-    backToTopLinkDiv.classList.add("d-none");
-    backToTopLinkDiv.classList.remove("d-block");
-  }
+	let backToTopLinkDiv = document.getElementById("back-to-top");
+	let location = document.body.getBoundingClientRect();
+	if (Math.abs(location.top) > offset) {
+		backToTopLinkDiv.classList.add("d-block");
+		backToTopLinkDiv.classList.remove("d-none");
+	} else {
+		backToTopLinkDiv.classList.add("d-none");
+		backToTopLinkDiv.classList.remove("d-block");
+	}
 });
 document.getElementById("back-to-top").addEventListener("click", function () {
-  window.scroll({top: 0, left: 0, behavior: "smooth"});
-  return false;
+	window.scroll({ top: 0, left: 0, behavior: "smooth" });
+	return false;
 });
