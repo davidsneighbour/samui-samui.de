abe1e4935c2bfb569612d7b986d2f918365740be
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-08-18 14:02:56 +0700

    chore(config): fix versioning setup
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

---
+++ .versionrc.cjs
@@ -0,0 +1,18 @@
+const defaultStandardVersion = require('@davidsneighbour/release-config');
+const localStandardVersion = {
+	bumpFiles: [
+		{
+			filename: "package.json",
+			type: "json",
+		},
+		{
+			filename: "assets/data/build.json",
+			type: "json",
+		},
+	],
+};
+const standardVersion = {
+	...defaultStandardVersion,
+	...localStandardVersion,
+};
+module.exports = standardVersion;
