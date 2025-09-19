8b22f66d425f66903162be39e770725639ff8573
Patrick Kollitsch <davidsneighbourdev@gmail.com>
2025-09-19 07:21:46 +0700

    chore(cleanup): move files between legacy and astro
    Signed-off-by: Patrick Kollitsch <davidsneighbourdev@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import netlify from '@astrojs/netlify';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), alpinejs()],
+  adapter: netlify(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
