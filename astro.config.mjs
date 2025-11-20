5e9b22fa7ff488b483da47fe01f36a96a5ffcc80
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-11-20 14:35:58 +0700

    fix: lots of smaller changes
    Signed-off-by: Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,72 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import redirects from './src/data/redirects.json';
+import icon from 'astro-icon';
+import pagefind from './src/scripts/integrations/pagefind.ts';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  // @ts-expect-error - env variable typing not recognized
+  compressHTML: import.meta.env.PROD,
+  redirects: redirects,
+  output: 'static',
+  prefetch: { defaultStrategy: 'viewport', prefetchAll: true },
+  server: { host: true },
+  build: {
+    format: 'directory',
+    assets: 'assets',
+    // @see https://docs.astro.build/en/reference/configuration-reference/#buildinlinestylesheets
+    inlineStylesheets: `auto`,
+    // assetsPrefix: 'https://cdn.example.com'
+  },
+  site: 'https://samui-samui.de',
+
+  integrations: [
+    mdx(),
+    sitemap({
+      namespaces: { image: false, news: false, video: false, xhtml: false },
+      xslURL: '/feeds/sitemap.xsl',
+    }),
+    icon({
+      iconDir: 'src/assets/icons',
+      svgoOptions: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                removeComments: { preservePatterns: false },
+                removeDoctype: true,
+              },
+            },
+          },
+        ],
+      },
+    }),
+    pagefind({ indexConfig: { keepIndexUrl: true } }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
   },
-  
+
+  experimental: {
+    chromeDevtoolsWorkspace: true,
+    clientPrerender: true,
+    contentIntellisense: true,
+    preserveScriptOrder: true,
+  },
+
+  image: {
+    breakpoints: [640, 750, 828, 1080, 1280],
+    layout: 'constrained',
+    objectFit: 'cover',
+    objectPosition: 'center',
+    responsiveStyles: true,
+  },
+
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,17 +3,24 @@ import rss from '@astrojs/rss';
 import setup from '@data/setup.json';
 
 export async function GET(context) {
-	const posts = await getCollection('posts');
-	return rss({
-		title: setup.title,
-		description: setup.description,
-		site: context.site,
-		items: posts.map((post) => {
-			const normalizedUrl = (post.data.url ?? post.id).replace(/^\/+|\/+$/g, '');
-			return {
-				...post.data,
-				link: `/${normalizedUrl}/`,
-			};
-		}),
-	});
+  let posts = await getCollection('posts', ({ data }) => {
+    return data?.draft !== true;
+  });
+  posts.sort((a, b) => Date.parse(b.data.date) - Date.parse(a.data.date));
+  posts = posts.slice(0, 10);
+  return rss({
+    description: setup.description,
+    items: posts.map((post) => {
+      const normalizedUrl = (post.data.url ?? post.id).replace(/^\/+|\/+$/g, '');
+      return {
+        categories: post.data.tags || [],
+        description: post.data.description,
+        link: `/${normalizedUrl}/`,
+        pubDate: post.data.date,
+        title: post.data.title,
+      };
+    }),
+    site: context.site,
+    title: setup.title,
+  });
 }
