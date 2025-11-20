d424fe30687cb42f670f9bf5a93f01d20ae3ec74
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-11-20 13:08:13 +0700

    fix: move from constants.ts to setup.json
    Signed-off-by: Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,19 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '@data/constants';
+import setup from '@data/setup.json';
 
 export async function GET(context) {
-const posts = await getCollection('cPosts');
-return rss({
-title: SITE_TITLE,
-description: SITE_DESCRIPTION,
-site: context.site,
-items: posts.map((post) => {
-const normalizedUrl = (post.data.url ?? post.id).replace(/^\/+|\/+$/g, '');
-return {
-...post.data,
-link: `/${normalizedUrl}/`,
-};
-}),
-});
+	const posts = await getCollection('cPosts');
+	return rss({
+		title: setup.title,
+		description: setup.description,
+		site: context.site,
+		items: posts.map((post) => {
+			const normalizedUrl = (post.data.url ?? post.id).replace(/^\/+|\/+$/g, '');
+			return {
+				...post.data,
+				link: `/${normalizedUrl}/`,
+			};
+		}),
+	});
 }
